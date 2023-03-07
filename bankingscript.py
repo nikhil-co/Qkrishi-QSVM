2cb7183ecdf2a9f0d3b42f46ec7347ca4d89d6a2
Nikhil Londhe <57909804+nikhil-co@users.noreply.github.com>
2023-03-07 23:25:49 +0530

    further testing

--- bankingscript.py
+++ bankingscript.py
@@ -1,31 +1,20 @@
-
-#Math Part
 import numpy as np
 import pandas as pd
 import time
-
-#Graph
 import matplotlib.pyplot as plt
-
-import importlib
-import circuit
-import encoding
 import qsvm
 import fitness
 import gsvm
-importlib.reload(circuit)
-importlib.reload(encoding)
-importlib.reload(qsvm)
-importlib.reload(fitness)
-importlib.reload(gsvm)
+
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, recall_score, accuracy_score
 
 
-nqubits = 4
+nqubits = 3
 depth = 3
 output="bank_testdata.csv"
 df = pd.read_csv('bank_cleaned.csv')
 
-bank_data = df.sample(n=2000,random_state=1)
+bank_data = df.sample(n=20000)#,random_state=1)
 #bank_data = np.around(bank_data)
 
 y = bank_data['y'].values
@@ -35,12 +24,12 @@ start = time.time()
 
 pop, pareto, logbook = gsvm.gsvm(nqubits=nqubits, depth=depth, nparameters=2,
                                     X=X, y=y, weights=[-1.0,1.0],
-                                    mu=50,lambda_=20,ngen=20,mutpb=0,cxpb=1)
+                                    mu=10,lambda_=5,ngen=5,mutpb=0.25,cxpb=.75)
+sim_time = time.time()
 
-print(f'Simulation finished after {time.time()-start} seconds')
-
-print(f'\nGenetic-algorithm output ({output})')
+print(f'Simulation finished after {sim_time-start} seconds')
 print('---------------------------------------------')
+
 with open(output, "w") as f:
     for ide, ind in enumerate(pareto):
         genes=''.join(str(i) for i in list(ind))
@@ -48,7 +37,7 @@ with open(output, "w") as f:
         line = f'{ide},"{genes}",{gates},{acc}'
         f.write(line)
         f.write('\n')
-        print(line)
+        #print(line)
 
 iot_result = pd.read_csv('bank_testdata.csv',header=None)
 
@@ -60,157 +49,64 @@ def ordenar_salidas_pareto(dataframe):
     return dataframe
 
 iot_salidas = ordenar_salidas_pareto(iot_result)
-#iot_salidas # Queremos ver el primer circuito del dataframe yya ordenado por accuracy (mejor ind)
-
-
-# from qiskit.circuit import ParameterVector, QuantumCircuit
-# from qiskit import QuantumRegister
-
-# def coding_bits(b):
-#     c = [b[n:n+5] for n,i in enumerate(b) if n%5==0]
-#     c_p=[]
-#     coding_0=[]
-#     for i in range(len(c)):
-#         for j in c[i]:
-#             c_p.append(str(j))
-#     np.asarray(c_p)
-#     c = [c_p[n:n+5] for n,i in enumerate(c_p) if n%5==0]
-#     for i in c:
-#         coding_0.append(''.join(i))
-#     return coding_0
-
-# class CircuitConversor:
-
-#     def __init__(self, nqubits, nparameters):
-#         gates = {}
-#         for n, suffix in enumerate(['00','01','10','11']):
-#             angle = np.pi / (2**n)
-#             gates['000'+suffix] = (self.make_H(), 1.0)
-#             gates['001'+suffix] = (self.make_cx(), 2.0)
-#             gates['010'+suffix] = (self.make_id(), 0.0)
-#             gates['011'+suffix] = (self.make_rx(angle), 1.0)
-#             gates['100'+suffix] = (self.make_rz(angle), 1.0)
-#             gates['101'+suffix] = (self.make_id(), 0.0)
-#             gates['110'+suffix] = (self.make_id(), 0.0)
-#             gates['111'+suffix] = (self.make_ry(angle), 1.0)
-#         self.gates = gates
-#         self.nqubits = nqubits
-#         self.register = QuantumRegister(nqubits, 'q')
-#         self.nparameters = nparameters
-#         self.nqubits = nqubits
-#         self.x = ParameterVector('x', nparameters)
-
-#     def __call__(self, coding_0):
-#         print(coding_0)
-#         circuit = QuantumCircuit(self.register)
-#         k = 0
-#         cost = 0
-#         for ndx, z in enumerate(coding_0):
-#             qubit = ndx % self.nqubits
-#             target = (ndx + 1) % self.nqubits
-#             fn, weight = self.gates[z]
-#             k = fn(circuit, k, qubit, target)
-#             cost += weight
-#         for i in range(k, self.nparameters):
-#             circuit.rz(self.x[i]*0, self.register[0])
-#         return circuit, cost
-
-#     def make_id(self):
-#         def operation(circuit, k, qubit, target):
-#             return k
-#         return operation
-
-#     def make_H(self):
-#         def operation(circuit, k, qubit, target):
-#             circuit.h(self.register[qubit])
-#             return k
-#         return operation
-
-#     def make_cx(self):
-#         def operation(circuit, k, qubit, target):
-#             circuit.cx(self.register[qubit], self.register[target])
-#             return k
-#         return operation
-
-#     def make_rx(self, angle):
-#         def operation(circuit, k, qubit, target):
-#             circuit.rx(self.x[k%self.nparameters] * angle,
-#                        self.register[qubit])
-#             return k+1
-#         return operation
-
-#     def make_ry(self, angle):
-#         def operation(circuit, k, qubit, target):
-#             circuit.ry(self.x[k%self.nparameters] * angle,
-#                        self.register[qubit])
-#             return k+1
-#         return operation
-
-#     def make_rz(self, angle):
-#         def operation(circuit, k, qubit, target):
-#             circuit.rz(self.x[k%self.nparameters] * angle,
-#                        self.register[qubit])
-#             return k+1
-#         return operation
-
-# cc = CircuitConversor(nqubits=nqubits, nparameters=2)
-# fm, puertas = cc(coding_bits(iot_salidas.circ[0]))
-# print(puertas)
-# fm.draw(output='mpl')
-
-
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, recall_score, accuracy_score
-import matplotlib.pyplot as plt
 
 def featuremap_performance(pop:str,nqubits:int) -> None:
     '''Returns the performance of a feature map on all of the dataset'''
 
     df = pd.read_csv('bank_cleaned.csv')
-    bank_data = df.sample(n = 10000)
+    df = df.sample(frac=1)
+    # bank_data = df.sample(n = 10000)
 
-    y = bank_data['y'].values
-    X = bank_data[['age','job','marital','education','default','balance',
-                'housing','loan','contact','day','month','duration','campaign','pdays','previous','poutcome']].values
+    for i in range(4):
+        if i == 7:
+            bank_data = df.iloc[60000:79844]
+            break
+        bank_data = df.iloc[i*20000:i*20000+20000]
+
+        y = bank_data['y'].values
+        X = bank_data[['age','job','marital','education','default','balance',
+                    'housing','loan','contact','day','month','duration','campaign','pdays','previous','poutcome']].values
 
 
-    fitness_obj = fitness.Fitness(nqubits,2,X,y,debug=True)
+        fitness_obj = fitness.Fitness(nqubits,2,X,y,debug=True)
 
-    training_features, training_labels, test_features, test_labels = fitness.Dataset(X,y)
+        training_features, training_labels, test_features, test_labels = fitness.Dataset(X,y)
 
-    model = qsvm.QSVM(lambda parameters: fitness_obj.cc(pop, parameters)[0],training_features,training_labels)#fitness_obj(pop)
+        model = qsvm.QSVM(lambda parameters: fitness_obj.cc(pop, parameters)[0],training_features,training_labels)#fitness_obj(pop)
 
-    y_pred = model.predict(test_features)
+        y_pred = model.predict(test_features)
 
-    cm = confusion_matrix(test_labels, y_pred)
+        cm = confusion_matrix(test_labels, y_pred)
 
-    #cm_display = ConfusionMatrixDisplay(cm).plot()
-    ConfusionMatrixDisplay.from_predictions(test_labels, y_pred)
-    plt.show()
-    recall = recall_score(test_labels, y_pred)
-    acc  = accuracy_score(test_labels, y_pred)
+        #cm_display = ConfusionMatrixDisplay(cm).plot()
+        ConfusionMatrixDisplay.from_predictions(test_labels, y_pred)
+        plt.show()
+        recall = recall_score(test_labels, y_pred)
+        acc  = accuracy_score(test_labels, y_pred)
 
-    print(f'String = {pop},\n accuracy = {acc}, recall = {recall}')
-    
+        print(f'String = {pop},\n accuracy = {acc}, recall = {recall} for {i+1}th section of the data')
+        
     return None
 
+print(f'Performance testing finished after {time.time()-start} seconds')
 featuremap_performance(iot_salidas.circ[0],nqubits)
 
 
 gen = logbook.select("gen")
-wc = logbook.chapters["wc"].select("media")
-acc = logbook.chapters["acc"].select("media")
+wc = logbook.chapters["wc"].select("min")
+acc = logbook.chapters["acc"].select("max")
 
 
 fig, ax1 = plt.subplots()
 plt.figure(dpi = 100)
-line1 = ax1.plot(gen, wc ,"b-", label="Average Weight Control")
+line1 = ax1.plot(gen, wc ,"b-", label="Min Weight Control")
 ax1.set_xlabel("Generation")
 ax1.set_ylabel("Weight Control", color="b")
 for tl in ax1.get_yticklabels():
     tl.set_color("b")
 
 ax2 = ax1.twinx()
-line2 = ax2.plot(gen, acc, "r-", label="Average Accuracy")
+line2 = ax2.plot(gen, acc, "r-", label="Max Accuracy")
 ax2.set_ylabel("Accuracy", color="r")
 for tl in ax2.get_yticklabels():
     tl.set_color("r")
@@ -218,5 +114,3 @@ for tl in ax2.get_yticklabels():
 lns = line1 + line2
 labs = [l.get_label() for l in lns]
 ax1.legend(lns, labs, loc="best")
-
-plt.show()
